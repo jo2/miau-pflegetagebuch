6f405835e066950af9bbfc1f233ac99dc739567e
Marius <marius.wagner@adesso.de>
2020-06-17 14:04:51 +0200

    CompleteTaskHandler

--- pflegetagebuch-skill/src/main/java/de/fhdo/pflegetagebuch/handlers/CompleteTaskHandler.java
+++ pflegetagebuch-skill/src/main/java/de/fhdo/pflegetagebuch/handlers/CompleteTaskHandler.java
@@ -3,10 +3,14 @@ package de.fhdo.pflegetagebuch.handlers;
 import com.amazon.ask.dispatcher.request.handler.HandlerInput;
 import com.amazon.ask.dispatcher.request.handler.RequestHandler;
 import com.amazon.ask.model.Response;
-import com.amazon.ask.model.Slot;
 import com.amazon.ask.request.RequestHelper;
+import de.fhdo.pflegetagebuch.domain.HealthStatus;
+import de.fhdo.pflegetagebuch.domain.SupportNeeded;
 import de.fhdo.pflegetagebuch.domain.Task;
+import de.fhdo.pflegetagebuch.services.TaskHandlerService;
+import de.fhdo.pflegetagebuch.util.Util;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 import static com.amazon.ask.request.Predicates.intentName;
@@ -21,9 +25,22 @@ public class CompleteTaskHandler implements RequestHandler {
     public Optional<Response> handle(HandlerInput handlerInput) {
         RequestHelper helper = RequestHelper.forHandlerInput(handlerInput);
         Task task = new Task();
-        Slot slot = helper.getSlot("task").get();
+        
+        String taskName = Util.getContentFromSlot("task", helper);
+        String healthStatusValue = Util.getContentFromSlot("healtStatus", helper);
+        HealthStatus healthStatus = HealthStatus.valueOf(healthStatusValue);
+        String supportNeededValue = Util.getContentFromSlot("supportNeeded", helper);
+        SupportNeeded supportNeeded = SupportNeeded.valueOf(supportNeededValue);
+        task.setName(taskName);
+        task.setHealthStatus(healthStatus);
+        task.setSupportNeeded(supportNeeded);
+        task.setCompletionDate(LocalDateTime.now());
+        TaskHandlerService taskHandlerService = new TaskHandlerService();
+        taskHandlerService.completeTask(task);
+
         return handlerInput.getResponseBuilder()
-                .withSpeech("Du Sohn deiner Mutter")
+                .withReprompt("")
+                .withSpeech("Kann ich sonst noch etwas für Sie erledigen")
                 .build();
     }
 }
